db0cb34047e7e18ccffa72ba5033a65d2bb3b73f
Seon JiHwan <tjswlghks0722@naver.com>
2024-03-12 20:40:55 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.15 ms, Memory: 70.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = new int[id_list.length];
+        Map<String, HashSet<String>> reportMap = new HashMap<>();
+        for(String name: id_list){
+            reportMap.put(name, new HashSet<>());
+        }
+        for(String s: report){
+            reportMap.get(s.split(" ")[1]).add(s.split(" ")[0]);
+        }
+        for(int i = 0; i < id_list.length; i++){
+            if(reportMap.get(id_list[i]).size() >= k){
+                HashSet<String> reportUser = reportMap.get(id_list[i]);
+                for(String s : reportUser){
+                    for(int j = 0; j < id_list.length; j++){
+                        if(id_list[j].equals(s)){
+                            answer[j]++;
+                        }
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
