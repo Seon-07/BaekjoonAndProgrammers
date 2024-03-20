2efadb1b8e8ae5d392cda617b0ae1a4b1fbbe232
Seon JiHwan <tjswlghks0722@naver.com>
2024-03-20 21:50:30 +0900

    [level 2] Title: 기능개발, Time: 2.55 ms, Memory: 69.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] works = new int[progresses.length];
+        List<Integer> result = new ArrayList<>();
+        Queue<Integer> que = new ArrayDeque<>();
+        for(int i = 0; i < progresses.length; i++){
+            int remain = 100 - progresses[i];
+            int workDay=(remain%speeds[i]!= 0)?remain/speeds[i]+1:remain/speeds[i];
+            que.offer(workDay);
+        }
+        int k = que.poll();
+        int cnt = 1;
+        while(!que.isEmpty()){
+            if(que.peek() > k){
+                result.add(cnt);
+                cnt = 1;
+                k = que.poll();
+            }else{
+                cnt++;
+                que.poll();
+            }
+        }
+        result.add(cnt);
+        
+        int[] answer = result.stream().mapToInt(i -> i).toArray();
+        return answer;
+    }
+}
