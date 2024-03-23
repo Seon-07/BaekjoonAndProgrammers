2a48d8364d64aa4eb6604e9c4f083eaf8d630eb1
Seon JiHwan <tjswlghks0722@naver.com>
2024-03-23 22:07:20 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.73 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] numbers) {
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int i = 0; i < numbers.length; i++){
+            for(int j = i+1; j < numbers.length; j++){
+                set.add(numbers[i]+numbers[j]);
+            }
+        }
+        int[] answer = new int[set.size()];
+        List<Integer> list = new ArrayList<>(set);
+        list.sort(null);
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
