2b9ae4bcc7f9ba41e90cbd8b166b987feba65fa0
Seon JiHwan <tjswlghks0722@naver.com>
2024-03-31 12:50:37 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.04 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n, int[] numlist) {
+        
+        List<Integer> li = new ArrayList<>();
+        for(int i = 0; i < numlist.length; i++){
+            if(numlist[i] % n == 0){
+                li.add(numlist[i]);
+            }
+        }
+        int[] answer = new int[li.size()];
+        for(int i = 0; i < li.size(); i++){
+            answer[i] = li.get(i);
+        }
+        return answer;
+    }
+}
