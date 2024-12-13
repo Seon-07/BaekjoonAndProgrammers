897e2025affa0483da15493c8f621bda1461c428
Seon <tjswlghks0722@naver.com>
2024-12-13 21:08:28 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 228 ms, Memory: 18324 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,1 @@
+import java.util.*;class Main{public static void main(String[]a){Scanner s=new Scanner(System.in);int[] k={1,1,2,2,2,8};for(int i=0;i<6;i++){k[i]-=s.nextInt();}for(int j:k){System.out.print(j+" ");}}}
