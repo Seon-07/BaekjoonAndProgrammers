f85fb5268ff17e7b9517c067f8e83c1a6a0c369c
Seon <tjswlghks0722@naver.com>
2025-08-10 13:22:31 +0900

    [Bronze IV] Title: 주사위 세개, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,12 @@
+A, B, C = map(int, input().split())
+if A == B and B == C :
+    print(10000 + (A * 1000))
+elif A != B and A != C and B != C :
+    print(max(A, B, C)*100)
+else :
+    if A == B :
+        print(1000 + (A*100))
+    elif B == C :
+        print(1000 + (B*100))
+    else:
+        print(1000 + (A*100))
