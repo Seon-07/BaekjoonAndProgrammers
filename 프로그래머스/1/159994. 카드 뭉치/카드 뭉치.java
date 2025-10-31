94f6f13bc98708cadb60c1dca206d8331f7516b8
Seon <tjswlghks0722@naver.com>
2025-10-31 23:01:59 +0900

    [level 1] Title: 카드 뭉치, Time: 16.74 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        int i = 0;
+        int j = 0;
+        for(String word : goal){
+            if(cards1.length-1 >= i && cards1[i].equals(word)){
+                i++;
+                continue;
+            }
+            if(cards2.length-1 >= j && cards2[j].equals(word)){
+                j++;
+                continue;
+            }
+            System.out.println(i + " " + j);
+            return "No";
+        }
+        return "Yes";
+    }
+}
